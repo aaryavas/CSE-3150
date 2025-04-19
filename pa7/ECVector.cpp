85e87e4fd67503167eff9bc12831a14293378be7
Aarya <aarya.vasantlal@uconn.edu>
2025-04-19 00:22:53 -0400

    base class initilaizor

--- pa7/ECVector.cpp
+++ pa7/ECVector.cpp
@@ -1,1 +1,15 @@
 #include "ECVector.h"
+
+
+//implement the ECVector class based off the header file
+//providing a definition for each member function
+
+
+//initializer base class
+//will only initialize the base class and non static data members
+template <typename T>
+//need to intialize the type <T> in the base since templated class
+ECVector<T>::ECVector(vector<T> _coordinates)
+    :coordinates(_coordinates),dimension(coordinates.size())
+    {}
+
