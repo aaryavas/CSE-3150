58b0b6c212dbd6a57b687ee213094afdfbba2c7e
Aarya <aarya.vasantlal@uconn.edu>
2025-05-02 16:00:45 -0400

    MediaCatalog.h Functionality

--- pa8/MediaCatalog.h
+++ pa8/MediaCatalog.h
@@ -18,12 +18,30 @@ private:
     double price;
 };
 
+using namespace std;
 class MediaCatalog {
 public:
-    ... // TODO: add methods
+    // TODO: add methods
+    //we want to return a single thing
+    static MediaCatalog& getInstance();
 
+    bool addMedia(const string& title, shared_ptr<Media>); //add method
+    
+    bool removeMedia(const string& title);
+    bool isAvailable(const string& title) const;
+    shared_ptr<Media> getMedia(const string& title);
+    
 private:
-    ... // TODO: add private members
+    // TODO: add private members
+    MediaCatalog();//default constructor
+    ~MediaCatalog(); //destructor
+    map<string, shared_ptr<Media>> inventory; //map for storing the media type and how much of that media type inventory is there
+    //one instance so we need to remove the copy costructors and operators
+    MediaCatalog(const MediaCatalog &other) = delete;
+    MediaCatalog& operator=(const MediaCatalog &other) = delete;
+
+
+
 };
 
 #endif
